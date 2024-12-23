7808451930425225fb2f87c13130519162e6ccf9
Isaiah Vickers <isaiah.vickers@outlook.com>
2024-12-23 12:52:01 -0500

    feat(routes): add upload routes to backend

--- backend/migrations/versions/20241223_121052_.py
+++ backend/migrations/versions/20241223_121052_.py
@@ -8,6 +8,9 @@ Create Date: 2024-12-23 12:10:52.152172
 from alembic import op
 import sqlalchemy as sa
 
+import os
+environment = os.getenv("FLASK_ENV")
+SCHEMA = os.environ.get("SCHEMA")
 
 # revision identifiers, used by Alembic.
 revision = 'b3b50cfb8ac9'
@@ -36,6 +39,9 @@ def upgrade():
     sa.ForeignKeyConstraint(['owner_id'], ['users.id'], ),
     sa.PrimaryKeyConstraint('id')
     )
+    
+    if environment == "production":
+        op.execute(f"ALTER TABLE users SET SCHEMA {SCHEMA};")
     # ### end Alembic commands ###
 
 
